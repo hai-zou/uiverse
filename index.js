cf5169065faac54d55910e1b3b1c48c6a87136d3
zouhai <zou.hai@outlook.com>
2024-05-17 13:56:52 +0800

    build: update

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 
-const EL_PATH = path.resolve(process.cwd(), './src');
-const INDEX_PATH = path.resolve(process.cwd(), './');
-const INDEX_TPL_PATH = path.resolve(process.cwd(), './index.tpl');
-
-// 读取当前目录下的文件列表
-fs.readdir(EL_PATH, (err, files) => {
+// read dir
+fs.readdir('./src', (err, files) => {
   if (err) {
-    console.error('读取目录失败:', err);
+    console.error(err);
     return;
   }
-  const htmlTpl = String(fs.readFileSync(INDEX_TPL_PATH));
+  const htmlTpl = String(fs.readFileSync('./index.tpl'));
   const list = files.map(name => ({
     name,
-    url: `${EL_PATH}/${name}/index.html`
+    url: `./src/${name}/index.html`
   }));
   const resultTpl = htmlTpl.replace(/{{list}}/g, JSON.stringify(list));
 
-  fs.writeFileSync(`${INDEX_PATH}/index.html`, resultTpl);
+  fs.writeFileSync(`./index.html`, resultTpl);
 });
