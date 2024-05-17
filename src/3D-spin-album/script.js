dde005df1152d2cab764403c70dd8b935d3e74d0
zouhai <zou.hai@outlook.com>
2024-05-18 06:21:04 +0800

    feat: 鼠标滚轮缩放效果

--- src/3D-spin-album/script.js
+++ src/3D-spin-album/script.js
@@ -1,15 +1,26 @@
 const allImg = document.getElementsByTagName("img");
 const centerAxisEle = document.getElementById("center-axis");
 
+// 半径
+const radius = 60 * allImg.length;
+
+let x = -10;
+let y = 0;
+let z = -radius;
+
+centerAxisEle.style.transform = `translateZ(${z}px) rotateX(${x}deg) rotateY(${y}deg)`;
+
 Array.prototype.forEach.call(allImg, function (el, index) {
   // 旋转角度
   const rotateDeg = index * (360 / allImg.length);
-  // 半径
-  const radius = 25 * allImg.length;
   // 动画时间
   const animateTime = index * 0.1;
   el.style.transform = `rotateY(${rotateDeg}deg) translateZ(${radius}px)`;
   el.style.transition = `transform 1s ${animateTime}s`;
+
+  el.addEventListener('dragstart', (event) => {
+    event.preventDefault();
+  });
 });
 
 // 实现鼠标拖动效果
@@ -17,9 +28,6 @@ const useMouseMove = (callback) => {
   // 鼠标拖动偏移量
   let preX = null;
   let preY = null;
-  // 旋转角度
-  let angleX = 0;
-  let angleY = 0;
 
   const handleMouseMove = (event) => {
     if (preX === null || preY === null) {
@@ -27,10 +35,12 @@ const useMouseMove = (callback) => {
     } else {
       const offsetX = event.clientX - preX;
       const offsetY = event.clientY - preY;
-      angleX -= offsetY;
-      angleY += offsetX;
 
-      callback({ y: angleY, x: angleX });
+      //除以 10 ，降低灵敏度
+      x -= (offsetY / 10);
+      y += (offsetX / 10);
+
+      callback();
 
       preX = event.clientX;
       preY = event.clientY;
@@ -55,7 +65,24 @@ const useMouseMove = (callback) => {
   };
 };
 
-useMouseMove(({ x, y }) => {
-  //除以 10 ，降低灵敏度
-  centerAxisEle.style.transform = `rotateX(${x / 10}deg) rotateY(${y / 10}deg)`;
+// 鼠标滚轮缩放效果
+const useMouseWheel = (callback) => {
+  const handleMouseRoll = (event) => {
+    z += (event.deltaY < 0) ? 50 : -50;
+    callback();
+  };
+
+  document.addEventListener('wheel', handleMouseRoll);
+
+  return () => {
+    document.removeEventListener('wheel', handleMouseRoll);
+  };
+}
+
+useMouseMove(() => {
+  centerAxisEle.style.transform = `translateZ(${z}px) rotateX(${x}deg) rotateY(${y}deg)`;
+});
+
+useMouseWheel(() => {
+  centerAxisEle.style.transform = `translateZ(${z}px) rotateX(${x}deg) rotateY(${y}deg)`;
 });
